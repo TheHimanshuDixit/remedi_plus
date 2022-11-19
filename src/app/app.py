987e0ddd956f9d2a08a0b4f2fec320a897045397
deadaf <gahlyanrohit18@gmail.com>
2022-11-19 15:37:28 +0530

    include all routers

--- src/app/app.py
+++ src/app/app.py
@@ -6,12 +6,17 @@ from tortoise.contrib.fastapi import register_tortoise
 import constants
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from .routes.reminders import router as reminders_router
+from .routes.medicine import router as medicine_router
+from .routes.test import router as test_router
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="src/static"), name="static")
+app.include_router(reminders_router)
+app.include_router(medicine_router)
+app.include_router(test_router)
 
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 template = Jinja2Templates(directory="src/templates")
 
 
@@ -20,9 +25,9 @@ async def root(request: Request):
     return template.TemplateResponse("index.html", {"request": request})
 
 
-@app.exception_handler(404)
-async def not_found(request: Request, exc):
-    return template.TemplateResponse("404.html", {"request": request})
+# @app.exception_handler(404)
+# async def not_found(request: Request, exc):
+#     return template.TemplateResponse("404.html", {"request": request})
 
 
 register_tortoise(
