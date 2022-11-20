51e4d923950cfa98cd74c363129a7a5393f279ec
deadaf <gahlyanrohit18@gmail.com>
2022-11-20 14:42:33 +0530

    some parsing in med search API

--- src/app/routes/medicine.py
+++ src/app/routes/medicine.py
@@ -14,4 +14,15 @@ async def get_med_details(request: Request, name: str):
     async with aiohttp.ClientSession() as session:
         async with session.get(config("MED_API") + name) as response:
             data = await response.json()
-            return data
+            drug_group = data["drugGroup"]
+            if not drug_group.get("conceptGroup"):
+                return []
+
+            _list = []
+            for concept in drug_group["conceptGroup"]:
+                if not concept.get("conceptProperties"):
+                    continue
+
+                _list.append(concept["conceptProperties"])
+
+            return _list
